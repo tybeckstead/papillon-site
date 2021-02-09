52105d7d5da99922f29c97aec02e6958cf86e34c
Ty <tybeck91@gmail.com>
2021-02-09 06:44:03 -0500

    Added Footer to Theme

--- themes/papillon-theme/footer.php
+++ themes/papillon-theme/footer.php
@@ -12,19 +12,74 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'pappilon-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'pappilon-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'pappilon-theme' ), 'pappilon-theme', '<a href="http://tylerbeckstead.com">Tyler Beckstead</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="grid-container">
+			<div class="grid-x">
+				<div class="large-3 cell footer_info">
+					<h3>Help</h3>
+					<ul class="foot_list">
+						<li class="foot_item"><p>Contact Us</p></li>
+						<li class="foot_item"><p>Returns & Exchange</p></li>
+						<li class="foot_item"><p>Shipping Policy</p></li>
+						<li class="foot_item"><p>Returns & Exchanges</p></li>
+						<li class="foot_item"><p>FAQs</p></li>
+					</ul>
+				</div>
+				<div class="large-3 cell footer_info">
+					<h3>The Company</h3>
+					<ul class="foot_list">
+						<li class="foot_item"><p>About Papillon</p></li>
+						<li class="foot_item"><p>Legal</p></li>
+						<li class="foot_item"><p>Careers</p></li>
+						<li class="foot_item"><p>Privacy & Cookies</p></li>
+						<li class="foot_item"><p>Code of Ethics</p></li>
+					</ul>
+				</div>
+				<div class="large-3 cell footer_info">
+					<h3>Follow Us</h3>
+					<ul class="foot_list">
+						<li class="foot_item">
+							<img class="footer_icon" src="./papillon-theme/assets/img/facebook.png" alt="Facebook">
+							<p>Facebook</p>
+						</li>
+						<li class="foot_item">
+							<img class="footer_icon" src="./papillon-theme/assets/img/twitter.png" alt="Twitter">
+							<p>Twitter</p>
+						</li>
+						<li class="foot_item">
+							<img class="footer_icon" src="./papillon-theme/assets/img/instagram.png" alt="Instagram">
+							<p>Instagram</p>
+						</li>
+						<li class="foot_item">
+							<img class="footer_icon" src="./papillon-theme/assets/img/youtube.png" alt="YouTube">
+							<p>YouTube</p>
+						</li>
+						<li class="foot_item">
+							<img class="footer_icon" src="./papillon-theme/assets/img/snapchat.png" alt="Snapchat">
+							<p>Snapchat</p>
+						</li>
+					</ul>
+				</div>
+				<div class="large-3 cell footer_info">
+					<h3>Our Newsletter</h3>
+					<p id="news_text">By entering your email address below, you conset to recieving our newsletter with access to our latest collections, events, and initiatives. More details on this are provided in our <a href="#">Privacy Policy</a>.</br><b>As explained in the <a href="#">Privacy Policy</a>, you may <a href="#">unsubscribe</a> at any time to withdraw you conset to recieve papillon electronic communications.</b></p>
+					<form>
+						<input type="text" placeholder="ENTER EMAIL ADDRESS" id="newsletter">
+						<p id="arrow">></p>
+					</form>
+				</div>
+			</div>
+			<div class="site-info">
+				<div class="grid-x">
+					<div class="cell large-4 small-12 rights">
+						<p>© 2021 Papillon - All Rights Reserved</p>
+					</div>
+					<div class="cell large-7 small-12 rights">
+						<p>Privacy Policy | Terms and Conditions | Cookies Policy</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- themes/papillon-theme/header.php
+++ themes/papillon-theme/header.php
@@ -23,7 +23,6 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pappilon-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="nav">
@@ -60,9 +59,9 @@
 
 				</div>
 				<div class="cell large-3 medium-3 main_icons">
-					<img src="/assets/img/search.png" alt="Search Icon" class="icons">
-					<img src="/assets/img/account.png" alt="Account Icon" class="icons">
-					<img src="/assets/img/cart.png" alt="Cart Icon" class="icons">
+					<img src="./assets/img/search.png" alt="Search Icon" class="icons">
+					<img src="./assets/img/account.png" alt="Account Icon" class="icons">
+					<img src="./assets/img/cart.png" alt="Cart Icon" class="icons">
 				</div>				
 				<div class="cell large-12 medium-12 navigation">
 					<ul class="nav_list vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown">
@@ -78,34 +77,4 @@
 				
 			</div>
 		</div>
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$pappilon_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $pappilon_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $pappilon_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pappilon-theme' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
