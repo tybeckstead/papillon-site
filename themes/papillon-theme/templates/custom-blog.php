43b89ed73b3bd23883e9ee35ebe8ff2c4e91d49a
Ty <tybeck91@gmail.com>
2021-03-08 18:37:18 -0500

    Blog Post Archive Styling

--- themes/papillon-theme/templates/custom-blog.php
+++ themes/papillon-theme/templates/custom-blog.php
@@ -13,7 +13,7 @@
 get_header();
 ?>
 <!-- MAKE UNQUE -->
-<main id="site-blog-post" role="main">
+<main id="post" role="main">
 	
 	<?php
 
@@ -24,38 +24,45 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			$args = array(
-				'posts_per_page' => 6, 
-				'offset' => 3,
-				'post_type' => 'post',
-				'post_status' => 'publish',
-			);
-		
-			$query = new WP_Query( $args );
-            
-			if ( $query->have_posts() ) {
-		
-				while ( $query->have_posts() ) {
-					$query->the_post();
-					
-					$post_title = get_the_title();
-					$post_content = get_the_excerpt();
-					$post_thumbnail = get_the_post_thumbnail_url();
-					$post_link = get_the_permalink();
-		
-					?>
+            ?>
 
-                    <div class="entry-content additional-post">
-                        <hr class="divider">
-                        <h2><a href="<?php echo $post_link; ?>"><?php echo $post_title;?></a></h2>
-                        <img class="wp-post-image" src="<?php echo $post_thumbnail; ?>" alt="Featured Post Image"  >
-                        <p><?php echo $post_content;?></p>
-                        <p><a href="<?php echo $post_title?>"> Read More</a></p>
-                    </div> 
-					
-					<?php
-				}
-			}
+            <main class="site_blog_content">
+                <?php
+                    $args = array(
+                        'posts_per_page' => 6, 
+                        'offset' => 1,
+                        'post_type' => 'post',
+                        'post_status' => 'publish',
+                    );
+                
+                    $query = new WP_Query( $args );
+                    
+                    if ( $query->have_posts() ) {
+                
+                        while ( $query->have_posts() ) {
+                            $query->the_post();
+                            
+                            $post_title = get_the_title();
+                            $post_content = get_the_excerpt();
+                            $post_thumbnail = get_the_post_thumbnail_url();
+                            $post_link = get_the_permalink();
+                
+                            ?>
+
+                            <div class="blog-content">
+                                <hr class="divider">
+                                <h4 class="blog_post_title"><a href="<?php echo $post_link; ?>"><?php echo $post_title;?></a></h4>
+                                <img class="wp_blog_img" src="<?php echo $post_thumbnail; ?>" alt="Featured Post Image"  >
+                                <p class="blog_paragraph"><?php echo $post_content;?></p>
+                                <p class="read_more"><a href="<?php echo $post_title?>"> Read More</a></p>
+                            </div> 
+                            
+                            <?php
+                        }
+                    }
+                ?>
+            </main>
+            <?php
 			wp_reset_postdata();
 		}
 	}
