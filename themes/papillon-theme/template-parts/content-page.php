4ec6f24a562e3d10ef67ccedc87f6cd351d3a33f
Ty <tybeck91@gmail.com>
2021-03-26 02:02:28 -0400

    Added Conditional Statement for Entry Header from content-page

--- themes/papillon-theme/template-parts/content-page.php
+++ themes/papillon-theme/template-parts/content-page.php
@@ -10,11 +10,48 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php pappilon_theme_post_thumbnail(); ?>
+	<?php if (is_front_page() ) {
+		?>
+		<div class="grid-x">
+			<div class="cell large-6 medium-6 small-12">
+				<?php if( has_post_thumbnail() ) { ?>
+					<div class="post-thumbnail main_thumb">
+						<img class="thumbnail_post_image" src="<?php echo esc_url( get_the_post_thumbnail_url() ); ?>">
+					</div>
+				<?php } ?>
+			</div>
+			<div class="cell large-6 medium-6 small-12">
+				<div class="center_align">
+					<h2 class="home_text">Dress in Style with Papillon</h2>
+					<h5 class="home_other_text">Check out our latest fashion collection</h5>
+					<p class="link"><a href="<?php echo esc_url( content_url( '/shop' ) ); ?>">SHOP WOMEN’S LATEST ARRIVALS</a></p>
+				</div>
+			</div>
+			<div class="cell large-12 small-12">
+				<div class="grid-x grid-margin-x">
+					<div class="large-8 large-offset-2 ">
+						<ul class="flex_links">
+							<li class="item_home"><a href="<?php echo esc_url( content_url( '/shop' ) ); ?>">Women</a></li>
+							<li class="item_home"><a href="<?php echo esc_url( content_url( '/shop' ) ); ?>">Men</a></li>
+							<li class="item_home"><a href="<?php echo esc_url( content_url( '/shop' ) ); ?>">Kids</a></li>
+							<li class="item_home"><a href="<?php echo esc_url( content_url( '/shop' ) ); ?>">Sale</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php
+	}else {?> 
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<?php if( has_post_thumbnail() ) { ?>
+				<div class="post-thumbnail main_thumb">
+					<img class="thumbnail_post_image" src="<?php echo esc_url( get_the_post_thumbnail_url() ); ?>">
+				</div>
+			<?php } ?>
+		</header><!-- .entry-header -->
+	<?php }?>
+	
 
 	<div class="entry-content">
 		<?php
